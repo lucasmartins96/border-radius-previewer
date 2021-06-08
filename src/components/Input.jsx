cce6b6ccdec4b852342e28064792e76ebac11c49
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-07 23:12:36 -0300

    implementando o componente Input

---
+++ src/components/Input.jsx
@@ -0,0 +1,38 @@
+import React /* , { Component } */ from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import changeSideBorder from '../redux/actions';
+
+function Input({ sideBorder, borderRadius, updateBorderRadius }) {
+  return (
+    <input
+      type="number"
+      className="form-control"
+      name={sideBorder}
+      value={borderRadius[sideBorder]}
+      onChange={({ target: { name, value } }) =>
+        updateBorderRadius(name, value)
+      }
+    />
+  );
+}
+
+const mapStateToProps = ({ borderRadiusReducer }) => ({
+  borderRadius: borderRadiusReducer,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  updateBorderRadius: (side, value) => dispatch(changeSideBorder(side, value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Input);
+
+Input.propTypes = {
+  sideBorder: PropTypes.string,
+  borderRadius: PropTypes.objectOf(PropTypes.string).isRequired,
+  updateBorderRadius: PropTypes.func.isRequired,
+};
+
+Input.defaultProps = {
+  sideBorder: 'whatever side',
+};
