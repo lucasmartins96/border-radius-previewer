160601607c2d5df64fcf4321af008d6912f5f03a
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-19 11:35:30 -0300

    adicionando o header da aplicação

---
+++ src/components/Describe.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Describe() {
+  return (
+    <header className="component-container" id="Describe">
+      <h1>Border Radius Previewer</h1>
+      <p>A propriedade border-radius permite inserir cantos arredondados aos elementos!</p>
+      <p>Experimente alterar os valores nos inputs em cada canto para modificar a propriedade na forma ao centro e visualize como elemento ficaria com a propriedade aplicada.</p>
+      <p>Ao clicar no botão, gere a propriedade CSS da forma e copie para o seu código.</p>
+    </header>
+  )
+}
+
+export default Describe;
